b84a135141e4b5f4817fe22b17a64e925f26d93e
BuildTools <thecelofyz@gmail.com>
2017-07-14 03:18:09 +0200

    Adding Player and preparing for more objects

--- Carnival/src/pl/everfree/celofyz/Handler.java
+++ Carnival/src/pl/everfree/celofyz/Handler.java
@@ -1,7 +1,20 @@
 package pl.everfree.celofyz;
 
+import java.awt.Graphics;
+import java.util.LinkedList;
+
 public class Handler {
 
+	LinkedList<GameObject> object = new LinkedList<GameObject>();
 	
+	public void tick(){
+		for(int i = 0; i < object.size(); i++){
+			GameObject tempObject = object.get(i);
+			tempObject.tick();
+		}
+	}
 	
+	public void render(Graphics g){
+		
+	}
 }
---
+++ Carnival/src/pl/everfree/celofyz/character/Player.java
@@ -0,0 +1,24 @@
+package pl.everfree.celofyz.character;
+
+import java.awt.Graphics;
+
+import pl.everfree.celofyz.GameObject;
+import pl.everfree.celofyz.ID;
+
+public class Player extends GameObject{
+
+	public Player(int x, int y, ID id) {
+		super(x, y, id);
+	}
+
+	@Override
+	public void tick() {	
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		
+	}
+
+}
